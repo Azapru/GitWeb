38f75e97ef5e3ceab770248a0a6029195624576c
Azapru <62551802+Azapru@users.noreply.github.com>
2024-01-04 22:46:13 +0100

    Progress!!!

--- Main.gd
+++ Main.gd
@@ -1,17 +1,43 @@
 extends Control
 
-var Text : PackedScene = preload("res://Elements/Text.tscn")
+var Paragraph : PackedScene = preload("res://Elements/Paragraph.tscn")
 var elements = []
+var gwml: Array
+
+var element: String
+var element_id = 0
+var properties: Array
+var builder_y = 0
 
 func _ready():
-	elements.append(Text.instantiate())
-	add_child(elements[0])
-	elements[0].text = "Balls"
+	# Load GWML file
+	gwml = FileAccess.open("test.gwml", FileAccess.READ).get_as_text().split("\n", false)
 	
-	elements.append(Text.instantiate())
-	add_child(elements[1])
-	elements[1].text = "Balls\nGaming!!\njireohqgpihr9pguiqh4"
-	elements[1].position = Vector2(0, 64)
+	# Build site
+	for line: String in gwml:
+		if line == "#\r":
+			if element == "paragraph":
+				elements.append(Paragraph.instantiate())
+				add_child(elements[element_id])
+				
+				elements[element_id].position.y = builder_y
+				
+				for property in properties:
+					if property[0] == "padding":
+						elements[element_id].position.x += int(property[1])
+						elements[element_id].position.y += int(property[1])
+						builder_y += int(property[1])
+					if property[0] == "text":
+						elements[element_id].text = property[1]
+				
+				builder_y += elements[element_id].size.y
+				
+			element_id += 1
+		elif line == "#paragraph\r":
+			element = "paragraph"
+			properties = []
+		else:
+			properties.append(line.split("=", false, 1))
 	
 func _process(delta):
 	pass
